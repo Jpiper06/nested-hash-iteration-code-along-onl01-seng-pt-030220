d89f27c66b8f0c408865d3bcb2c73e6965742ee1
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 21:42:07 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,14 +1,6 @@
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
-    end
-  end
-end
-
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         binding.pry
       end
